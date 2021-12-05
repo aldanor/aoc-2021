08c6b34df18911c7c65ddc3345ebd6a3a63111b9
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-12-05 18:35:06 +0000

    Day 05 solution, initial line sweep version

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
-// pub mod day05;
+pub mod day05;
 // pub mod day06;
 // pub mod day07;
 // pub mod day08;
--- src/runner.rs
+++ src/runner.rs
@@ -4,7 +4,7 @@ use std::time::{Duration, Instant};
 use eyre::{ensure, Result};
 use structopt::StructOpt;
 
-const N_DAYS: usize = 4;
+const N_DAYS: usize = 5;
 
 const W_DAY: usize = 10;
 const W_PART: usize = 10;
@@ -64,7 +64,7 @@ fn run_one_raw(day: u8, part: u8, times: usize) -> (Duration, String) {
     if_day!(2, day02);
     if_day!(3, day03);
     if_day!(4, day04);
-    // if_day!(5, day05);
+    if_day!(5, day05);
     // if_day!(6, day06);
     // if_day!(7, day07);
     // if_day!(8, day08);
