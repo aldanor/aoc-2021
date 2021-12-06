8d3a9bdc66fa67dec40e9c787ab6d1d579ea2103
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-12-06 10:10:37 +0000

    Day 06 as dot products

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,8 +1,58 @@
+use std::ops::{AddAssign, Mul};
+
 use crate::utils::*;
 
 #[inline]
 pub fn input() -> &'static [u8] {
     include_bytes!("input.txt")
+}
+
+type T = usize;
+
+#[inline(always)]
+const fn row_dot<const N: usize>(x: &[T; N], y: &[T; N]) -> T {
+    let (mut i, mut v) = (0, 0);
+    while i < N {
+        v += x[i] * y[i];
+        i += 1;
+    }
+    v
+}
+
+#[inline(always)]
+const fn dot<const N: usize>(m: &[[T; N]; N], x: &[T; N]) -> [T; N] {
+    let (mut i, mut v) = (0, [0; N]);
+    while i < N {
+        v[i] = row_dot(&m[i], x);
+        i += 1;
+    }
+    v
+}
+
+#[inline]
+pub fn solve16<const N16: usize>(mut s: &[u8]) -> usize {
+    let mut v = [0_usize; 9];
+    while s.len() > 1 {
+        v[(s.get_first() - b'0') as usize] += 1;
+        s = s.advance(2);
+    }
+
+    const M: [[usize; 9]; 9] = [
+        [2, 0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 2, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 2, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 2, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0, 2, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0, 1],
+    ];
+
+    for _ in 0..N16 {
+        v = dot(&M, &v);
+    }
+    v.into_iter().sum()
 }
 
 #[inline]
@@ -22,7 +72,7 @@ pub fn solve<const N: usize>(mut s: &[u8]) -> usize {
 
 #[inline]
 pub fn part1(s: &[u8]) -> usize {
-    solve::<80>(s)
+    solve16::<{ 80 >> 4 }>(s)
 }
 
 #[inline]
