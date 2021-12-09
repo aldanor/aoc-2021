5a0ac16b2a30bc9767fabd0dbe183ebee2e1361e
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-12-09 20:35:13 +0000

    Day 09 - speed up part 1 using SIMD

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -8,8 +8,55 @@ pub fn input() -> &'static [u8] {
     include_bytes!("input.txt")
 }
 
+macro_rules! lanes_min {
+    ($lhs:expr, $rhs:expr) => {
+        ($lhs).lanes_ge(($rhs)).select(($rhs), ($lhs))
+    };
+}
+
 #[inline]
 pub fn part1(mut s: &[u8]) -> usize {
+    type T = i8;
+    type S = core_simd::Simd<T, 64>;
+
+    let mut prev = [S::splat(T::MAX); 2];
+    let mut prev_min3 = [S::splat(T::MAX); 2];
+    let mut out = 0;
+
+    let zero = S::splat(0);
+    let threshold = S::splat((b'0' - 1) as _);
+
+    for i in 1..=(N + 2) {
+        let mut arr = [T::MAX; 128];
+
+        if i <= N {
+            unsafe { std::ptr::copy_nonoverlapping(s.as_ptr(), arr.as_mut_ptr().add(1).cast(), N) };
+            s = s.advance(N + 1);
+        }
+
+        for j in 0..2 {
+            let left = S::from_array(unsafe { *arr.as_ptr().add(0 + 64 * j).cast() });
+            let this = S::from_array(unsafe { *arr.as_ptr().add(1 + 64 * j).cast() });
+            let right = S::from_array(unsafe { *arr.as_ptr().add(2 + 64 * j).cast() });
+
+            let this_min3 = lanes_min!(left, right);
+            let this_min3 = lanes_min!(this_min3, prev[j]);
+
+            if i >= 2 {
+                let min_cross = lanes_min!(this, prev_min3[j]);
+                let value = prev[j] - threshold;
+                out += prev[j].lanes_lt(min_cross).select(value, zero).horizontal_sum() as usize;
+            }
+
+            prev[j] = this;
+            prev_min3[j] = this_min3;
+        }
+    }
+    out as _
+}
+
+#[allow(unused)]
+pub fn part1_naive(mut s: &[u8]) -> usize {
     let mut grid = [[0xff_u8; N + 2]; N + 2];
     for i in 1..=N {
         for j in 1..=N {
